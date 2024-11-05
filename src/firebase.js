75274a543c4d012738a0c867c472369fe3b0551f
thevu1000 <thevu7000@gmail.com>
2024-11-05 11:26:54 +0700

    fIX lỗi database

--- src/firebase.js
+++ src/firebase.js
@@ -8,8 +8,8 @@ import { getDatabase } from "firebase/database"
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  databaseURL: "https://mementomori-vn-default-rtdb.firebaseio.com",
+  projectId: "mementomori-vn",
   storageBucket: "mementomori-vn.appspot.com",
   messagingSenderId: "369747595092",
   appId: "1:369747595092:web:61c7744dc3eae6d70642c1",
